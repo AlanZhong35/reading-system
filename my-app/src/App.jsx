3051a461c00944f45ce2f8a7c7565398fbbd65a4
Zhong <AZhong@badgermeter.com>
2024-07-15 10:53:14 -0500

    added routing for salary predictor and booktracker, todo - make home page and local storage for booktracker

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import './App.css'
 import Dashboard from './Dashboard';
+import Home from './Home.jsx';
+import BookTracker from "./BookTracker.jsx";
+import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes, Link, NavLink  } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <Dashboard/>
+        <Router>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand as={NavLink} to="/reading-system">Alan's Website</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link as={NavLink} to="/reading-system/salary-predictor" activeClassName="active">NBA Salary Predictor</Nav.Link>
+                        <Nav.Link as={NavLink} to="/reading-system/book-tracker" activeClassName="active">Book Tracker</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Routes>
+                <Route path="/reading-system/" element={<Home/>}/>
+                <Route path="/reading-system/salary-predictor" element={<Dashboard/>}/>
+                <Route path="/reading-system/book-tracker" element={<BookTracker/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </Router>
     </>
-  )
+  );
+}
+
+function NotFound() {
+    return <h1>Not Found</h1>;
 }
 
 export default App
---
+++ my-app/src/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>welcome home</h1>
+        </div>
+    );
+};
+
+export default Home;
