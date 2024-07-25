4d92671c2a553cf460a6b2a6c665de12aa6ea874
Zhong <AZhong@badgermeter.com>
2024-07-25 15:59:01 -0500

    made the navbar stay at the top

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 import Dashboard from './Dashboard';
-import Home from './Home.jsx';
-import BookTracker from "./BookTracker.jsx";
-import { Navbar, Nav } from 'react-bootstrap';
+import Home from './Home';
+import BookTracker from "./BookTracker";
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes, NavLink  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 
 function App() {
-//tried to stick the navbar to the top with inline style
-  return (
-    <>
+    return (
         <Router>
-            <Navbar bg="light" expand="lg">
-            <Navbar.Brand as={NavLink} to="/reading-system">Alan's Website</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link as={NavLink} to="/reading-system/salary-predictor" >NBA Salary Predictor</Nav.Link>
-                        <Nav.Link as={NavLink} to="/reading-system/book-tracker" >Book Tracker</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+            <Navbar  fixed="top" bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand as={NavLink} to="/reading-system">Alan's Website</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link as={NavLink} to="/reading-system/salary-predictor">NBA Salary Predictor</Nav.Link>
+                            <Nav.Link as={NavLink} to="/reading-system/book-tracker">Book Tracker</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
-            <Routes>
-                <Route path="/reading-system" element={<Home/>}/>
-                <Route path="/reading-system/salary-predictor" element={<Dashboard/>}/>
-                <Route path="/reading-system/book-tracker" element={<BookTracker/>}/>
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
+            <Container className="content">
+                <Routes>
+                    <Route path="/reading-system/" element={<Home />} />
+                    <Route path="/reading-system/salary-predictor" element={<Dashboard />} />
+                    <Route path="/reading-system/book-tracker" element={<BookTracker />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Container>
         </Router>
-    </>
-  );
+    );
 }
 
 function NotFound() {
     return <h1>Not Found</h1>;
 }
 
-export default App
+export default App;
